33cfbab788c916a8258b2bf4c34b47f7241f33c3
Bala <balaganesh172004@gmail.com>
2025-12-01 11:32:19 +0530

    push

---
+++ javaproject/src/com/index/ControlNested.java
@@ -0,0 +1,19 @@
+package com.index;
+
+public class ControlNested {
+
+	public static void main(String[] args) {
+	int age=34;
+	if(age>=18)
+	{
+		if(age<=40)
+		{
+			System.out.println("your are Uncle");
+		}
+	}
+	
+	
+
+	}
+
+}
