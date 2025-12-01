6858c17681a25f1b6d74cc29cfbb9ddc4cae0288
Bala <balaganesh172004@gmail.com>
2025-12-01 11:13:05 +0530

    First commit for java

---
+++ javaproject/src/com/index/ControlElse.java
@@ -0,0 +1,18 @@
+package com.index;
+
+public class ControlElse {
+
+	public static void main(String[] args) {
+		int age=20;
+		if(age>=18)
+		{
+			System.out.println("Eligible");
+			
+		}
+		else
+		{
+			System.out.println("Not Eligible");
+		}
+	}
+
+}
